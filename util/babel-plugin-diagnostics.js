746a56026318dabb04844a6d29899493681b5131
Josh Pullen <JoshPullen27@gmail.com>
2021-12-04 15:02:01 -0500

    Update babel plugin to log inside of functions

--- util/babel-plugin-diagnostics.js
+++ util/babel-plugin-diagnostics.js
@@ -20,99 +20,107 @@ module.exports = function (babel) {
                 t.identifier("__console")
               ),
             ],
-            t.stringLiteral(state.opts.diagnosticsURL)
+            t.stringLiteral(state.opts.diagnosticsURL || "https://example.com/")
           )
         );
       },
-      "ExpressionStatement|VariableDeclaration|ReturnStatement"(path) {
-        // Check if this statement is part of a list
-        // such as the top-level of the program, or
-        // inside a function declaration
-        if (!path.inList) {
-          return;
-        }
+      "ExpressionStatement|VariableDeclaration|ReturnStatement": {
+        /*
+          For some reason the statements found inside of inline functions
+          (functions defined within some other ExpressionStatement) don't
+          work unless this runs on exit(). I don't understand Babel well
+          enough to know why.
+        */
+        exit(path) {
+          // Check if this statement is part of a list
+          // such as the top-level of the program, or
+          // inside a function declaration
+          if (!path.inList) {
+            return;
+          }
 
-        if (t.isExpressionStatement(path)) {
-          if (t.isCallExpression(path.get("expression"))) {
-            if (
-              path
-                .get("expression.callee")
-                .isIdentifier({ name: "__reportValuesAfterStatement" }) ||
-              path
-                .get("expression.callee")
-                .isIdentifier({ name: "__reportValuesBeforeStatement" })
-            ) {
-              return;
-            }
+          if (t.isExpressionStatement(path)) {
+            if (t.isCallExpression(path.get("expression"))) {
+              if (
+                path
+                  .get("expression.callee")
+                  .isIdentifier({ name: "__reportValuesAfterStatement" }) ||
+                path
+                  .get("expression.callee")
+                  .isIdentifier({ name: "__reportValuesBeforeStatement" })
+              ) {
+                return;
+              }
 
-            if (
-              path.get("expression.callee").isIdentifier({ name: "__report" })
-            ) {
-              return;
+              if (
+                path.get("expression.callee").isIdentifier({ name: "__report" })
+              ) {
+                return;
+              }
             }
           }
-        }
 
-        // It feels like there should be a built-in way to
-        // do this, but I didn't immediately see anything
-        // so I just wrote my own.
-        function getAllVarsInScope(scope) {
-          let varsInScope = Object.keys(scope.bindings);
-          if (scope.parent) {
-            varsInScope.push(...getAllVarsInScope(scope.parent));
+          // It feels like there should be a built-in way to
+          // do this, but I didn't immediately see anything
+          // so I just wrote my own.
+          function getAllVarsInScope(scope) {
+            let varsInScope = Object.keys(scope.bindings);
+            if (scope.parent) {
+              varsInScope.push(...getAllVarsInScope(scope.parent));
+            }
+            return varsInScope;
           }
-          return varsInScope;
-        }
 
-        const varsInScope = getAllVarsInScope(path.scope);
+          const varsInScope = getAllVarsInScope(path.scope);
 
-        const createReportValuesCode = (
-          funcName,
-          startLine,
-          endLine,
-          varNames
-        ) => {
-          return t.expressionStatement(
-            t.callExpression(t.identifier(funcName), [
-              t.numericLiteral(startLine),
-              t.numericLiteral(endLine),
-              t.arrowFunctionExpression(
-                [t.identifier("__report")],
-                t.blockStatement(
-                  varNames.map((name) =>
-                    t.tryStatement(
-                      t.blockStatement([
-                        t.expressionStatement(
-                          t.callExpression(t.identifier("__report"), [
-                            t.stringLiteral(name),
-                            t.identifier(name),
-                          ])
-                        ),
-                      ]),
-                      t.catchClause(t.identifier("e"), t.blockStatement([]))
+          const createReportValuesCode = (
+            funcName,
+            startLine,
+            endLine,
+            varNames
+          ) => {
+            return t.expressionStatement(
+              t.callExpression(t.identifier(funcName), [
+                t.numericLiteral(startLine),
+                t.numericLiteral(endLine),
+                t.arrowFunctionExpression(
+                  [t.identifier("__report")],
+                  t.blockStatement(
+                    varNames.map((name) =>
+                      t.tryStatement(
+                        t.blockStatement([
+                          t.expressionStatement(
+                            t.callExpression(t.identifier("__report"), [
+                              t.stringLiteral(name),
+                              t.identifier(name),
+                            ])
+                          ),
+                        ]),
+                        t.catchClause(t.identifier("e"), t.blockStatement([]))
+                      )
                     )
                   )
-                )
-              ),
-            ])
-          );
-        };
+                ),
+              ])
+            );
+          };
 
-        path.replaceWithMultiple([
-          createReportValuesCode(
-            "__reportValuesBeforeStatement",
-            path.node.loc.start.line,
-            path.node.loc.end.line,
-            varsInScope
-          ),
-          path.node,
-          createReportValuesCode(
-            "__reportValuesAfterStatement",
-            path.node.loc.start.line,
-            path.node.loc.end.line,
-            varsInScope
-          ),
-        ]);
+          path.replaceWithMultiple([
+            createReportValuesCode(
+              "__reportValuesBeforeStatement",
+              path.node.loc.start.line,
+              path.node.loc.end.line,
+              varsInScope
+            ),
+            path.node,
+            createReportValuesCode(
+              "__reportValuesAfterStatement",
+              path.node.loc.start.line,
+              path.node.loc.end.line,
+              varsInScope
+            ),
+          ]);
+        },
       },
     },
   };
